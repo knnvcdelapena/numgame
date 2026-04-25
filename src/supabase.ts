6aea5a95c4b09af525e67661bd0effd0e9fa7c92
Kennevic <kennevicdelapena@gmail.com>
2026-04-25 23:49:18 +0800

    debug env vars

--- src/supabase.ts
+++ src/supabase.ts
@@ -4,3 +4,12 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+console.log('url:', supabaseUrl)
+console.log('key:', supabaseAnonKey)
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
