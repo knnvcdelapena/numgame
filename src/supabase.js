47d559af0a7977207b2f5b3d43e59da3fa15143e
Kennevic <kennevicdelapena@gmail.com>
2026-04-25 23:59:43 +0800

    fix supabase init

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log('url:', supabaseUrl);
+console.log('key:', supabaseAnonKey);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
