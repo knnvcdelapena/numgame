4e269a5c5990e0dc2b486646e44dddf1dda70ac6
Kennevic <kennevicdelapena@gmail.com>
2026-04-25 23:52:57 +0800

    force rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Set this to your GitHub repo name for gh-pages deployment
-  // e.g. base: '/my-numgame/'
+  // force rebuild
   base: './',
   build: {
     outDir: 'dist',
